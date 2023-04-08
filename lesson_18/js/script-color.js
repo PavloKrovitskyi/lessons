bbf365ee57e66bc1c0256c3394123d0c68e8b792
PavloKrovitskyi <123174653+PavloKrovitskyi@users.noreply.github.com>
2023-04-08 13:08:56 +0200

    Add files via upload

--- lesson_18/js/script-color.js
+++ lesson_18/js/script-color.js
@@ -1,11 +1,10 @@
 window.onload = function () {
   const burgerIcon = document.querySelector('.menu-burger');
   const logo = document.querySelector('.header__logo');
-  const sidebarLink = document.querySelector('.menu__sidebar-link');
+
 
   burgerIcon.addEventListener('click', function () {
     logo.classList.toggle('white');
-    sidebarLink.classList.toggle('white');
   });
 };
 
