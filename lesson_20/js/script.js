f41cd9f24e207bff62b7ef31949b15f3f04c16bb
PavloKrovitskyi <123174653+PavloKrovitskyi@users.noreply.github.com>
2023-04-26 17:24:08 +0200

    Add files via upload

---
+++ lesson_20/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("block-products");
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("block-products").style.marginLeft = "0";
+}
